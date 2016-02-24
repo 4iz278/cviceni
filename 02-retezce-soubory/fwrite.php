ba55c5188dbc53005f8be73f37929d0eb0957ccd
Stanislav Vojíř <stanislav.vojir@gmail.com>
2016-02-24 18:23:24 +0100

    fwrite

--- 02-retezce-soubory/fwrite.php
+++ 02-retezce-soubory/fwrite.php
@@ -1,2 +1,18 @@
 <?php
-  //TODO
+
+  $f = fopen("file.txt", "w");
+  if ($f){
+
+    fputs($f, "radek dat"); //zapíše do souboru textový řádek (tj. se zalomením řádku)
+
+    fwrite($f, "data"); //zapíše do souboru binární data
+
+    fwrite($f, "data", 100); //zapíše do souboru binární data, pokud jsou kratší, než zadaná délka, dojde k doplnění mezerami
+
+    fclose($f); //zavře soubor, dozapíše data, která zůstala v cache
+
+    //pokud chcete na nějaké místě kódu vynutit reálný zápis do souboru, lze to zařídit pomocí fflush($f;)
+
+  }else{
+    echo 'Soubor se nepodařilo otevřít';
+  }
