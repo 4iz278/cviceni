625454ef4b2b45f46d311b5d5a261efac92f39e8
Stanislav Vojíř <stanislav.vojir@gmail.com>
2016-03-02 19:09:35 +0100

    dědičnost, traity1

--- 03-objekty/03-objekty-dedicnost.php
+++ 03-objekty/03-objekty-dedicnost.php
@@ -1,4 +1,67 @@
 <?php
 
+/** Jednoduchý příklad definice třídy a jejího použití */
 
-//XXX
+/**
+ * Class Clovek
+ * @property string $jmeno //z hlediska dokumentac je jedno, jestli public proměnné definujete takto, nebo v rámci dokumentace jednotlivých proměnných
+ * @property string $prijmeni
+ */
+class Clovek{
+  public $jmeno;
+  public $prijmeni;
+  /** @var string[] $poznamky  */
+  private $poznamky=[]; //private proměnná je přístupná jen v rámci dané třídy (ne v rámci potomků)
+  /** @var string $id = '' */
+  protected $id = ''; //protected proměnnou nebude možné překrýt v rámci potomka
+
+  /**
+   * @param string $jmeno=''
+   * @param string $prijmeni=''
+   */
+  public function __construct($jmeno='', $prijmeni=''){
+    $this->jmeno=$jmeno;
+    $this->prijmeni=$prijmeni;
+    $this->generateId();
+  }
+
+  /**
+   * Metoda pro vygenerování nového id
+   */
+  final private function generateId(){ //pokud u definice metody použijeme klíčové slovo final, nebude ji možné překrýt v rámci potomka
+    $this->id = uniqid();
+  }
+
+  /**
+   * Metoda pro výpis daného objektu
+   * @return string
+   */
+  public function __toString(){
+    return $this->jmeno.' '.$this->prijmeni;
+  }
+
+}
+
+/**
+ * Class Student - ukázka rozšiřující třídy
+ */
+class Student extends Clovek{
+  protected $rocnik = 1;
+
+  /**
+   * Funkce pro výpis - překrývá
+   * @return string
+   */
+  public function __toString(){
+    return parent::__toString().' - rocnik '.$this->rocnik.' (id:'.$this->id.')';
+    //pokud chceme pracovat s překrytou metodou, můžeme se k ní dostat pomocí klíčového slova parent
+    //přístupné proměnné a metody (public nebo protected) jsou normálně dostupné, i když byly definovány v rodičovské třídě
+  }
+}
+
+
+
+$pepa = new Student('Josef','Novák');
+echo $pepa; //při výpisu se použije funkce __toString()
+
+
--- 03-objekty/03-objekty-traity-1.php
+++ 03-objekty/03-objekty-traity-1.php
@@ -1,3 +1,37 @@
 <?php
 
-//XXX
+  /**
+   * Trait MujTrait
+   */
+  trait MujTrait{
+    private $x = 'neco'; //v rámci definice traitu je možné definovat nejen metody, ale také proměnné (properties)
+
+    /**
+     * @return string
+     */
+    public function getX(){
+      return $this->x;
+    }
+
+    /**
+     * @return int
+     */
+    protected function getRandomNumber(){
+      return rand(1,10);
+    }
+  }
+
+  /**
+   * Class MojeTrida - ukázka třídy s traitem
+   */
+  class MojeTrida{
+
+    use MujTrait; //pomocí klíčového slova use můžeme načíst příslušný trait
+
+    public function echoX(){
+      echo $this->x; //proměnná $x byla sice definovaná v traitu jako private, ale je tu normálně dostupná (obsah traitu je "nakopírován" do této třídy)
+    }
+  }
+
+  $objekt=new MojeTrida();
+  $objekt->echoX();
