cc8803367dbd3b9ad5967b1df85a1a0c8a4e8fe5
Stanislav Vojíř <stanislav.vojir@gmail.com>
2016-04-16 02:48:51 +0200

    jsonserializable

--- 10-json-xml/json/jsonserializable.php
+++ 10-json-xml/json/jsonserializable.php
@@ -1,3 +1,56 @@
 <?php
 
-//TODO
+/**
+ * Class Osoba
+ * @property string $jmeno
+ * @property string $prijmeni
+ * @property int $id
+ */
+class Osoba implements JsonSerializable{
+    public $jmeno;
+    public $prijmeni;
+    public $id;
+    /** @var  string[] $komentare */
+    private $komentare;
+
+    /**
+     * Funkce pro serializaci do JSONu
+     * @return array
+     */
+    public function jsonSerialize(){
+        //tato funkce musí vracet obsah, který je možné dále serializovat - řetězec, pole, serializovatelný objekt atp.
+        //lze si vybrat, co se má serializovat a jak...
+        return [
+            'id' =>$this->id,
+            'jmeno'=>$this->jmeno,
+            'prijmeni'=>$this->prijmeni
+        ];
+    }
+
+    /**
+     * Konstruktor třídy Osoba
+     * @param null $id
+     * @param $jmeno
+     * @param $prijmeni
+     */
+    public function __construct($id=null,$jmeno,$prijmeni){
+        $this->id=$id;
+        $this->jmeno=$jmeno;
+        $this->prijmeni=$prijmeni;
+    }
+
+    /**
+     * Funkce pro přidání komentáře
+     * @param string $str
+     */
+    public function pridatKomentar($str){
+        $this->komentare[]=$str;
+    }
+}
+
+$pepa=new Osoba(10,'Josef','Novák');
+$pepa->pridatKomentar('lorem ipsum...');
+$pepa->pridatKomentar('lorem ipsum...');
+
+//serializace za využití námi definované funkce
+echo json_encode($pepa, JSON_UNESCAPED_UNICODE);
