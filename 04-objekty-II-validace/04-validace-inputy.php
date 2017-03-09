40e6d87c106c9db2a8506ae69f2459a1a770fd03
Stanislav Vojíř <stanislav.vojir@gmail.com>
2017-03-09 12:50:38 +0100

    oprava 04-validace-inputy.php

--- 04-objekty-II-validace/04-validace-inputy.php
+++ 04-objekty-II-validace/04-validace-inputy.php
@@ -32,7 +32,7 @@ if (!empty($_POST) && (@$_POST['akce']=='odeslano')){
   if (empty($chyby)){
     //pokud nebyly nalezeny chyby, tak uložíme získaná data a provedeme redirect
     file_put_contents('data.txt', $_POST['jmeno'].';'.$_POST['prijmeni'].';'.$_POST['email'].';'.$_POST['email'].';'.$_POST['phone-os']."\n",FILE_APPEND);
-    header('Location: 04-validace-souhrnna.php?saved=ok');
+    header('Location: 04-validace-inputy.php?saved=ok');
     exit();
   }
 
@@ -40,9 +40,10 @@ if (!empty($_POST) && (@$_POST['akce']=='odeslano')){
 
 /**
  * Funkce pro jednoduché vypsání chyby
+ * @param array $chyby
  * @param string $id
  */
-function vypisChyby($id){
+function vypisChyby(array $chyby, $id){
   if (!empty($chyby[$id])){
     echo '<div style="color:red;">'.$chyby[$id].'</div>';
   }
@@ -50,66 +51,67 @@ function vypisChyby($id){
 
 ?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/html">
-<head>
-  <title>Ukázka HTML validace</title>
-  <meta charset="UTF-8"/>
-  <meta http-equiv=X-UA-Compatible content="IE=edge" />
-</head>
-<body>
-
-<h1>Ukázka formuláře s validací 2</h1>
-
-<form method="post" action="04-validace-souhrnna.php">
-  <input type="hidden" name="akce" value="odeslano"/>
-  <table>
-    <tr>
-      <td><label for="jmeno">Jméno</label></td>
-      <td>
-        <input type="text" name="jmeno" id="jmeno" value="<?php echo htmlspecialchars(@$_POST['prijmeni']);?>" required />
-        <?php vypisChyby('jmeno');?>
-      </td>
-    </tr>
-    <tr>
-      <td><label for="prijmeni">Příjmení</label></td>
-      <td>
-        <input type="text" name="prijmeni" id="prijmeni" value="<?php echo htmlspecialchars(@$_POST['prijmeni']);?>" required />
-        <?php vypisChyby('prijmeni');?>
-      </td>
-    </tr>
-    <tr>
-      <td><label for="email">E-mail</label></td>
-      <td>
-        <input type="email" name="email" id="email" value="<?php echo htmlspecialchars(@$_POST['email']);?>" />
-        <?php vypisChyby('email');?>
-      </td>
-    </tr>
-    <tr>
-      <td><label for="phone">Telefon</label></td>
-      <td>
-        <input type="tel" name="phone" id="phone" value="<?php echo htmlspecialchars(@$_POST['phone']);?>" />
-        <?php vypisChyby('phone');?>
-      </td>
-    </tr>
-    <tr>
-      <td><label for="phone-os">Telefon - OS</label></td>
-      <td>
-        <select name="phone-os" id="phone-os">
-          <option value="">--vyberte</option>
-          <option value="android" <?php echo(@$_POST['phone-os']=='android'?'selected="selected"':'');?>>android</option>
-          <option value="ios" <?php echo(@$_POST['phone-os']=='ios'?'selected="selected"':'');?>>ios</option>
-          <option value="windows" <?php echo(@$_POST['phone-os']=='windows'?'selected="selected"':'');?>>windows</option>
-        </select>
-        <?php vypisChyby('phone-os');?>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <input type="submit" value="odeslat..." />
-      </td>
-    </tr>
-  </table>
-</form>
-
-
-</body>
+  <head>
+    <title>Ukázka HTML validace</title>
+    <meta charset="UTF-8"/>
+    <meta http-equiv=X-UA-Compatible content="IE=edge" />
+  </head>
+  <body>
+    <h1>Ukázka formuláře s validací 2</h1>
+    <?php
+         if (!empty($_GET['saved']) && ($_GET['saved']=='ok')){
+           echo '<p><em>Data byla uložena.</em></p>';
+         }
+    ?>  
+    <form method="post" action="04-validace-inputy.php">
+      <input type="hidden" name="akce" value="odeslano"/>
+      <table>
+        <tr>
+          <td><label for="jmeno">Jméno</label></td>
+          <td>
+            <input type="text" name="jmeno" id="jmeno" value="<?php echo htmlspecialchars(@$_POST['prijmeni']);?>" required />
+            <?php vypisChyby($chyby,'jmeno');?>
+          </td>
+        </tr>
+        <tr>
+          <td><label for="prijmeni">Příjmení</label></td>
+          <td>
+            <input type="text" name="prijmeni" id="prijmeni" value="<?php echo htmlspecialchars(@$_POST['prijmeni']);?>" required />
+            <?php vypisChyby($chyby,'prijmeni');?>
+          </td>
+        </tr>
+        <tr>
+          <td><label for="email">E-mail</label></td>
+          <td>
+            <input type="email" name="email" id="email" value="<?php echo htmlspecialchars(@$_POST['email']);?>" />
+            <?php vypisChyby($chyby,'email');?>
+          </td>
+        </tr>
+        <tr>
+          <td><label for="phone">Telefon</label></td>
+          <td>
+            <input type="tel" name="phone" id="phone" value="<?php echo htmlspecialchars(@$_POST['phone']);?>" />
+            <?php vypisChyby($chyby,'phone');?>
+          </td>
+        </tr>
+        <tr>
+          <td><label for="phone-os">Telefon - OS</label></td>
+          <td>
+            <select name="phone-os" id="phone-os">
+              <option value="">--vyberte</option>
+              <option value="android" <?php echo(@$_POST['phone-os']=='android'?'selected="selected"':'');?>>android</option>
+              <option value="ios" <?php echo(@$_POST['phone-os']=='ios'?'selected="selected"':'');?>>ios</option>
+              <option value="windows" <?php echo(@$_POST['phone-os']=='windows'?'selected="selected"':'');?>>windows</option>
+            </select>
+            <?php vypisChyby($chyby,'phone-os');?>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="submit" value="odeslat..." />
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
 </html>
