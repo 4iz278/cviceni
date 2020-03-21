fdd62e85781ca4b9624a1641a857624c4bcb0d72
Stanislav Vojíř <stanislav.vojir@gmail.com>
2020-03-21 22:59:56 +0100

    ukázková aplikace a prezentace s postupem její tvorby

---
+++ 05-sql-databaze/05-aplikace-nastenka/edit.php
@@ -0,0 +1,94 @@
+<?php
+
+  //načteme připojení k databázi
+  require_once 'inc/db.php';
+
+  $errors=[];
+  if (!empty($_POST)){
+    #region zpracování formuláře
+    #region kontrola kategorie
+    if (!empty($_POST['category'])){
+
+      $categoryQuery=$db->prepare('SELECT * FROM categories WHERE category_id=:category LIMIT 1;');
+      $categoryQuery->execute([
+        ':category'=>$_POST['category']
+      ]);
+      if ($categoryQuery->rowCount()==0){
+        $errors['category']='Zvolená kategorie neexistuje!';
+        $_POST['category']='';
+      }
+
+    }else{
+      $errors['category']='Musíte vybrat kategorii.';
+    }
+    #endregion kontrola kategorie
+    #region kontrola textu
+    $text=trim(@$_POST['text']);
+    if (empty($text)){
+      $errors['text']='Musíte zadat text příspěvku.';
+    }
+    #endregion kontrola textu
+
+    if (empty($errors)){
+      #region uložení dat
+      $saveQuery=$db->prepare('INSERT INTO posts (user_id, category_id, text) VALUES (:user, :category, :text);');
+      $saveQuery->execute([
+        ':user'=>1, //zatím uživatele nijak neřešíme (to bude předmětem dalšího cvičení)
+        ':category'=>$_POST['category'],
+        ':text'=>$text
+      ]);
+      #endregion uložení dat
+      #region přesměrování
+      header('Location: index.php');
+      exit();
+      #endregion přesměrování
+    }
+    #endregion zpracování formuláře
+  }
+
+  //vložíme do stránek hlavičku
+  $pageTitle='Nový příspěvek';
+  include 'inc/header.php';
+?>
+
+  <form method="post">
+
+    <div class="form-group">
+      <label for="category">Kategorie:</label>
+      <select name="category" id="category" required class="form-control <?php echo (!empty($errors['category'])?'is-invalid':''); ?>">
+        <option value="">--vyberte--</option>
+        <?php
+          $categoryQuery=$db->prepare('SELECT * FROM categories ORDER BY name;');
+          $categoryQuery->execute();
+          $categories=$categoryQuery->fetchAll(PDO::FETCH_ASSOC);
+          if (!empty($categories)){
+            foreach ($categories as $category){
+              echo '<option value="'.$category['category_id'].'" '.($category['category_id']==@$_POST['category']?'selected="selected"':'').'>'.htmlspecialchars($category['name']).'</option>';
+            }
+          }
+        ?>
+      </select>
+      <?php
+        if (!empty($errors['category'])){
+          echo '<div class="invalid-feedback">'.$errors['category'].'</div>';
+        }
+      ?>
+    </div>
+
+    <div class="form-group">
+      <label for="text">Text příspěvku:</label>
+      <textarea name="text" id="text" required class="form-control <?php echo (!empty($errors['category'])?'is-invalid':''); ?>"><?php echo htmlspecialchars(@$_POST['text'])?></textarea>
+      <?php
+        if (!empty($errors['text'])){
+          echo '<div class="invalid-feedback">'.$errors['text'].'</div>';
+        }
+      ?>
+    </div>
+
+    <button type="submit" class="btn btn-primary">uložit...</button>
+    <a href="index.php" class="btn btn-light">zrušit</a>
+  </form>
+
+<?php
+  //vložíme do stránek patičku
+  include 'inc/footer.php';
---
+++ 05-sql-databaze/05-aplikace-nastenka/index.php
@@ -0,0 +1,39 @@
+<?php
+  //načteme připojení k databázi
+  require_once 'inc/db.php';
+
+  //vložíme do stránek hlavičku
+  include 'inc/header.php';
+
+  $query = $db->prepare('SELECT
+                           posts.*, users.name AS user_name, users.email, categories.name AS category_name
+                           FROM posts JOIN users USING (user_id) JOIN categories USING (category_id) ORDER BY updated DESC;');
+  $query->execute();
+
+  $posts = $query->fetchAll(PDO::FETCH_ASSOC);
+  if (!empty($posts)){
+    #region výpis příspěvků
+    echo '<div class="row">';
+    foreach ($posts as $post){
+      echo '<article class="col-12 col-md-6 col-lg-4 col-xxl-3 border border-dark mx-1 my-1 px-2 py-1">';
+      echo '  <div><span class="badge badge-secondary">'.htmlspecialchars($post['category_name']).'</span></div>';
+      echo '  <div>'.nl2br(htmlspecialchars($post['text'])).'</div>';
+      echo '  <div class="small text-muted mt-1">';
+                echo htmlspecialchars($post['user_name']);
+                echo ' ';
+                echo date('d.m.Y H:i:s',strtotime($post['updated']));//datum získané z databáze převedeme na timestamp a ten pak do českého tvaru
+      echo '  </div>';
+      echo '</article>';
+    }
+    echo '</div>';
+    #endregion výpis příspěvků
+  }else{
+    echo '<div class="alert alert-info">Nebyly nalezeny žádné příspěvky.</div>';
+  }
+
+  echo '<div class="row my-3">
+          <a href="edit.php" class="btn btn-primary">Přidat příspěvek</a>
+        </div>';
+
+  //vložíme do stránek patičku
+  include 'inc/footer.php';
