5a5bfedf0902ff588cb28fe9ade8a33321da00fa
Stanislav Vojíř <stanislav.vojir@gmail.com>
2020-03-25 00:34:21 +0100

    todo

--- 06-session-cookies/06-db-app-clients/db.php
+++ 06-session-cookies/06-db-app-clients/db.php
@@ -1,5 +1,6 @@
 <?php
 //pripojeni do db na serveru eso.vse.cz
+//TODO v následujícím řádku uveďte vlastní jméno a heslo k DB
 $db = new PDO('mysql:host=127.0.0.1;dbname=VASXNAME;charset=utf8', 'VASXNAME', 'VASE HESLO DO MYSQL');
 
 //vyhazuje vyjimky v pripade neplatneho SQL vyrazu
