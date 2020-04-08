8477a08eb7f34af41f4994cbc84bab3fd2e57c51
Stanislav Vojíř <stanislav.vojir@gmail.com>
2020-04-09 00:45:35 +0200

    doplněn pt-2 k hlavnímu obsahu webu

--- 07-ukazkova-aplikace/07-nastenka-reseni-du/inc/header.php
+++ 07-ukazkova-aplikace/07-nastenka-reseni-du/inc/header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   </head>
   <body>
-  <header class="container bg-dark">
-    <h1 class="text-white py-4 px-2">Nástěnka</h1>
-  </header>
-    <main class="container">
+    <header class="container bg-dark">
+      <h1 class="text-white py-4 px-2">Nástěnka</h1>
+    </header>
+    <main class="container pt-2">
