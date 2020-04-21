94951dd8184e670c26533e4f97ad7803e74bbf30
Stanislav Vojíř <stanislav.vojir@gmail.com>
2020-04-21 17:28:31 +0200

    optimistic lock

--- 09-uzivatele-db-json-xml/09-app-eshop/update_optimistic.php
+++ 09-uzivatele-db-json-xml/09-app-eshop/update_optimistic.php
@@ -28,10 +28,25 @@
     $description=$_POST['description'];
     $price=floatval($_POST['price']);
 
+    /*
+     * OPTIMISTIC LOCK:
+     * Před uložením si vytáhneme z DB čas poslední změny. Pokud se tento liší od času předaného z formuláře (tj. času na začátku editace), znamená to, že se záznam mezitím v pozadí změnil. (Jiný uživatel provedl update. Mohl to být ale i ten samý uživatel např. v jiném okně prohlížeče.)
+     * V případě, že se záznam v mezičase změnil, je nutné se NĚJAK zachovat. Je možné uživatele varovat, nabídnout přeuložení, sloučení změn atd., nebo prostě jen umřít s hláškou, že záznam byl změněn.
+     *
+     * Proměnnou $_POST['last_updated_at'] si předáváme ve formuláři jako hidden pole.
+     */
 
+    if ($_POST['last_updated_at'] != $goods['last_updated_at']) {
+      //tady by měl ideálně být návrat na formulář s označenými daty, co se změnilo, a nabídnout přepis či ponechání dat
+      //TODO domácí úkol :)
+      exit("The goods were updated by someone else in meantime!");
+    }
+
+    //pokud se časy poslední editace záznam a čas z formuláře rovnají, tj. záznam nebyl mezitím změněn, můžeme provést update - tedy pokud formulář neobsahuje jiné chyby
+    //nakonec také zaktualizujeme čas poslední aktualizace uložený u daného zboží
     if (empty($formErrors)){
       #region uložení zboží do DB
-      $stmt = $db->prepare('UPDATE goods SET name=:name, description=:description, price=:price WHERE id=:id LIMIT 1;');
+      $stmt = $db->prepare('UPDATE goods SET name=:name, description=:description, price=:price, last_updated_at=now() WHERE id=:id LIMIT 1;');
       $stmt->execute([
         ':name'=> $name,
         ':description'=> $description,
@@ -63,24 +78,26 @@
       }
     ?>
 
-	<form method="post">
-    <label for="name">Name</label><br/>
-    <input type="text" name="name" id="name" value="<?php echo htmlspecialchars(@$name);?>" required><br/><br/>
+    <form method="post">
+      <label for="name">Name</label><br/>
+      <input type="text" name="name" id="name" value="<?php echo htmlspecialchars(@$name);?>" required><br/><br/>
 
-    <label for="price">Price<br/>
-    <input type="number" min="0" name="price" id="price" required value="<?php echo htmlspecialchars(@$price)?>"><br/><br/>
+      <label for="price">Price<br/>
+      <input type="number" min="0" name="price" id="price" required value="<?php echo htmlspecialchars(@$price)?>"><br/><br/>
 
-    <label for="description">Description</label><br/>
-    <textarea name="description" id="description"><?php echo htmlspecialchars(@$description)?></textarea><br/><br/>
-				
-		<br/>
-		
-		<input type="hidden" name="id" value="<?php echo $goods['id']; ?>" />
-		
-		<input type="submit" value="Save" /> or <a href="/">Cancel</a>
-		
-	</form>
+      <label for="description">Description</label><br/>
+      <textarea name="description" id="description"><?php echo htmlspecialchars(@$description)?></textarea><br/><br/>
 
-</body>
+      <br/>
 
+      <input type="hidden" name="id" value="<?php echo $goods['id']; ?>" />
+
+      <input type="hidden" name="last_updated_at" value="<?php echo $goods['last_updated_at']; ?>">
+      <!--hidden pole používáme pro předání informace o čase poslední změny záznamu-->
+
+      <input type="submit" value="Save" /> or <a href="index.php">Cancel</a>
+
+    </form>
+
+  </body>
 </html>
