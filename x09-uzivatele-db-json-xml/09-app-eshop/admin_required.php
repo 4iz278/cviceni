ac30ec4f4b101203b89f82f13f6513d65e1d419e
Stanislav Vojíř <stanislav.vojir@gmail.com>
2026-04-28 13:06:05 +0200

    odstraněny přesunuté soubory

--- x09-uzivatele-db-json-xml/09-app-eshop/admin_required.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  //nejprve si vynutíme, aby byl uživatel přihlášený
-  require 'user_required.php';
-
-  //ověříme, jestli je uživatel v roli admin - pokud ne, tak mu zabráníme v přístupu
-  if(empty($currentUser) || ($currentUser['role']!='admin')){
-    die('Tato stránka je dostupná pouze administrátorům.');
-  }
